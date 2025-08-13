865eb2e7e85721e0d5828735835cb54789a7fca5
Cyka <nachos636@gmail.com>
2025-08-13 12:11:56 +0300

    [UPDATE] [dotNetCompat] 0.0.1 -> 0.1.0
    [ADD] Extensions that extend classic .net API

--- Assets/dotNetCompat/Runtime/CollectionsMarshal.cs
+++ Assets/dotNetCompat/Runtime/CollectionsMarshal.cs
@@ -3,27 +3,18 @@
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
+using static System.Runtime.CompilerServices.MethodImplOptions;
+using static dotNetCompat.Extensions.Utilities;
+
 // ReSharper disable CheckNamespace
 namespace System.Runtime.InteropServices
 {
     public static class CollectionsMarshal
     {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [MethodImpl(AggressiveInlining)]
         public static Span<T> AsSpan<T>(List<T> list)
         {
-            var box = new ListCastHelper { List = list }.StrongBox!;
-
-            return new Span<T>((T[]) box.Value, 0, list.Count);
-        }
-
-        [StructLayout(LayoutKind.Explicit)]
-        private struct ListCastHelper
-        {
-            [FieldOffset(0)]
-            public StrongBox<Array> StrongBox;
-
-            [FieldOffset(0)]
-            public object List;
+            return new Span<T>(new ListCastHelper(list).GetArray<T>(), 0, list.Count);
         }
     }
 }
---
+++ Assets/dotNetCompat/Runtime/Extensions/CollectionsMarshalExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+using static System.Runtime.CompilerServices.MethodImplOptions;
+using static System.Runtime.InteropServices.LayoutKind;
+using static dotNetCompat.Extensions.Utilities;
+
+namespace dotNetCompat.Extensions
+{
+    /// <summary>
+    /// This API extends classic .net API
+    /// </summary>
+    public static class CollectionsMarshalExtensions
+    {
+        [MethodImpl(AggressiveInlining)]
+        public static ReadOnlySpan<T> AsReadOnlySpan<T>(this List<T> list)
+        {
+            return new ReadOnlySpan<T>(new ListCastHelper(list).GetArray<T>(), 0, list.Count);
+        }
+    }
+
+    internal static class Utilities
+    {
+        [StructLayout(Explicit)]
+        [SuppressMessage("ReSharper", "PrivateFieldCanBeConvertedToLocalVariable")]
+        public readonly ref struct ListCastHelper
+        {
+            [FieldOffset(0)] private readonly object _list;
+            [FieldOffset(0)] private readonly StrongBox<Array> _strongBox;
+
+            [MethodImpl(AggressiveInlining)]
+            public T[] GetArray<T>() => (T[]) _strongBox.Value;
+
+            public ListCastHelper(IList list)
+            {
+                _strongBox = default!;
+                _list = list;
+            }
+        }
+    }
+}
